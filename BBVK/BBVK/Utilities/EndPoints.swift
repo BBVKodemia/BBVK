0520e60c75be9661d1a0990eb1ea1011cb7dd0f4
jonathanavilap1 <40183554+jonathanavilap1@users.noreply.github.com>
2022-04-03 18:46:46 -0500

    fixing appi call issues

---
+++ BBVK/BBVK/Utilities/EndPoints.swift
@@ -0,0 +1,17 @@
+//
+//  EndPoints.swift
+//  BBVK
+//
+//  Created by Jonathan Avila on 03/04/22.
+//
+
+import Foundation
+
+
+struct BBVKEndPoints {
+   
+   static let registerEndPoint = "https://bankodemia.kodemia.mx/users"
+   
+   
+   
+}
